d64615f89f3b896eda3f60462f146a81662e4766
Tomas <horsekit1982@gmail.com>
2019-02-21 00:26:22 +0800

    update pet store link

--- src/App.js
+++ src/App.js
@@ -62,8 +62,9 @@ const cards = [
     image: 'https://truffleframework.com/img/tutorials/pet-shop/petshop.png',
     header: 'Block chain pet store',
     demo: 'http://pet-store.tomas.website/',
+    github: 'https://github.com/horsekitlin/pet-shop',
     members: [
-      {name: 'Tomas Lin', url: 'https://github.com/horsekitlin/pet-shop'}
+      {name: 'Tomas Lin', url: 'https://github.com/horsekitlin'}
     ],
     description: [
       '第一個Dapp 區塊鏈的寵物商店實作',
