a51ec177328d48b1f961abb99041d230499e4dc8
林暐秦 <linweiqin@linweiqinde-MacBook-Pro.local>
2020-07-30 21:33:55 +0800

    add gulp add .travis.yml

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,13 @@ const shell = require('gulp-shell');
 
 const destPath = `/var/www/html/home.tomas.website`;
 
-gulp.task('default', async () => {
-  return gulp
-    .src('*.js', { read: false })
-    .pipe(shell("yarn build"))
-    .pipe(shell(`mkdir -p ${destPath}`))
-    .pipe(shell(`cp -a ./build/**/* ${destPath}`));
+gulp.task('default', async (done) => {
+  console.log(process.env);
+  console.log(process.env.HOST);
+  done();
+  // return gulp
+  //   .src('*.js', { read: false })
+  //   .pipe(shell("yarn build"))
+  //   .pipe(shell(`mkdir -p ${destPath}`))
+  //   .pipe(shell(`cp -a ./build/**/* ${destPath}`));
 });
