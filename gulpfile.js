471f520a238d98a4adfdec0f536fa4a40d151a8b
林暐秦 <linweiqin@linweiqinde-MacBook-Pro.local>
2020-07-30 22:22:36 +0800

    auto deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell');
+const scp = require('gulp-scp2');
 
 const destPath = `/var/www/html/home.tomas.website`;
+const {
+  HOST,
+  DEST,
+  ACCOUNT,
+  PASSWORD,
+} = process.env;
 
-gulp.task('default', async (done) => {
+gulp.task('default', async () => {
   console.log(process.env);
   console.log(process.env.HOST);
-  done();
+  return gulp.src('./build/**')
+  .pipe(scp({
+    host: HOST,
+    username: ACCOUNT,
+    password: PASSWORD,
+    dest: DEST,
+  }))
+  .on('error', function(err) {
+    console.log(err);
+  });
   // return gulp
   //   .src('*.js', { read: false })
   //   .pipe(shell("yarn build"))
